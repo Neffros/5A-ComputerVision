6642addfb7835c08b49ad895e7aa070d842497c6
Lior <diler.lior@gmail.com>
2021-12-14 12:53:18 +0100

    non functionnal binding box

--- features/features.cpp
+++ features/features.cpp
@@ -26,12 +26,47 @@ cv::BFMatcher bfMatcher;
 std::vector<std::vector<cv::DMatch>> matches;
 std::vector<cv::DMatch> filteredMatches;
 
+std::vector<Point2f> objectEdges;
+
 cv::Mat output;
 
 void computeImage()
 {
     orb->detect(object, objectKeyPoints);
     orb->compute(object, objectKeyPoints, objectDescriptor);
+}
+
+std::vector<cv::Point2f> getImageEdges(const cv::Mat image)
+{
+    std::vector<cv::Point2f> res(4);
+
+    res[0] = cv::Point2f(0, 0);
+    res[1] = cv::Point2f(image.cols, 0);
+    res[2] = cv::Point2f(image.cols, image.rows);
+    res[3] = cv::Point2f(0, image.rows);
+
+    return res;
+}
+void drawBindingBox(cv::Mat& image)
+{
+    std::vector<Point2f> objectPoints;
+    std::vector<Point2f> scenePoints;
+    std::vector<Point2f> sceneEdges(4);
+
+    for (auto match : filteredMatches)
+    {
+        objectPoints.push_back(objectKeyPoints[match.queryIdx].pt);
+        scenePoints.push_back(sceneKeyPoints[match.trainIdx].pt);
+    }
+
+    cv::Mat homography = cv::findHomography(objectPoints, scenePoints); //cv::RANSAC
+    cv::perspectiveTransform(objectEdges, sceneEdges, homography);
+
+    //Dessine sur le flux vidéo
+    cv::line(image, sceneEdges[0], sceneEdges[1], (255, 255, 255), 5);
+    cv::line(image, sceneEdges[1], sceneEdges[2], (255,255,255), 5);
+    cv::line(image, sceneEdges[2], sceneEdges[3], (255, 255, 255), 5);
+    cv::line(image, sceneEdges[3], sceneEdges[0], (255, 255, 255), 5);
 }
 void computeVideo()
 {
@@ -41,15 +76,15 @@ void computeVideo()
     orb->detect(nextInput, sceneKeyPoints);
     orb->compute(nextInput, sceneKeyPoints, sceneDescriptor);
     bfMatcher.knnMatch(objectDescriptor, sceneDescriptor, matches, 2);
-    //bfMatcher.knnMatch(objectDescriptor, sceneDescriptor, matches,2);
-    //std::cout << matches[0][0].distance << " - " << matches[0][1].distance << std::endl;
+
     for (auto match : matches)
     {
-        if (match[0].distance < 0.55 * match[1].distance)
+        if (match[0].distance < 0.65 * match[1].distance)
         {
             filteredMatches.push_back(match[0]);
         }
     }
+
     //cv::imshow("object", nextInput);
     //cv::waitKey(0);
 }
@@ -62,12 +97,21 @@ void draw()
         cv::circle(img, keyPoint.pt, 5, cv::Scalar(255, 255, 255));
     }*/
 
-    cv::drawMatches(object, objectKeyPoints, nextInput, sceneKeyPoints, filteredMatches, img);
+    //cv::drawMatches(object, objectKeyPoints, nextInput, sceneKeyPoints, filteredMatches, img);
+    if (filteredMatches.size() > 6)
+    {
+        std::cout << "binding box" << std::endl;
+        drawBindingBox(img);
+    }
 
 
     //draw extra things here
     //cv::imshow("scene", output);
     cv::imshow("scene", img);
+    /*if (filteredMatches.size() > 6)
+    {
+        cv::waitKey();
+    }*/
 }
 
 void video(const std::string videoname = "")
@@ -109,12 +153,13 @@ int main()
         return -1;
     }
     computeImage();
+
     for (auto keyPoint : objectKeyPoints)
     {
         cv::circle(object, keyPoint.pt, 5, cv::Scalar(255, 255, 255));
     }
     cv::imshow("object", object);
-
+    objectEdges = getImageEdges(object);
     video(resourcePath + videoPath);
     //waitKey(0); // Wait for any keystroke in the window
 
