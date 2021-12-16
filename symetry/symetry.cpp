99a2259370b571a20b124444482de52e8e30c520
Lior <diler.lior@gmail.com>
2021-12-16 13:27:57 +0100

    part I of symetry

--- symetry/symetry.cpp
+++ symetry/symetry.cpp
@@ -5,46 +5,59 @@
 #include <iostream>
 #include <string>
 
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
+
+std::string resourcePath = "E:/dev/vision_par_ordinateur/5A-ComputerVision/symetry/resources/";
+std::string imageName = "f.jpg";
+
+float distanceCoeff = 0.65;
+
+cv::Ptr<cv::ORB> orb = cv::ORB::create(5000);
+cv::Ptr<cv::BFMatcher> bfMatcher = cv::BFMatcher::create();
+
+std::vector<cv::KeyPoint> imgKeyPoints;
+cv::Mat imgDescriptor;
+std::vector<cv::KeyPoint> flippedKeyPoints;
+cv::Mat flippedDescriptor;
+
+std::vector<std::vector<cv::DMatch>> matches;
+std::vector<cv::DMatch> filteredMatches;
+
+
+void computeImage(cv::Mat img, cv::Mat flippedImg)
+{
+	orb->detect(img, imgKeyPoints);
+	orb->compute(img, imgKeyPoints, imgDescriptor);
+
+	orb->detect(flippedImg, flippedKeyPoints);
+	orb->compute(flippedImg, flippedKeyPoints, flippedDescriptor);
+
+	bfMatcher->knnMatch(imgDescriptor, flippedDescriptor, matches, 2);
+	for (auto match : matches)
+	{
+		if (match[0].distance < distanceCoeff * match[1].distance)
+		{
+			filteredMatches.push_back(match[0]);
+		}
+	}
+}
 int main()
 {
-    std::cout << "PROJECT1" << std::endl;
-    std::string projectPath = "E:/dev/vision_par_ordinateur/5A-ComputerVision/disparity/resources/antoine.png";
-    // Read the image file
-    Mat image = imread(projectPath);
+	cv::Mat img = cv::imread(resourcePath + imageName);
+	cv::Mat flippedImg;
+	cv::Mat res;
 
-    if (image.empty()) // Check for failure
-    {
-        cout << "Could not open or find the image" << endl;
-        system("pause"); //wait for any key press
-        return -1;
-    }
+	cv::flip(img, flippedImg, 1);
 
-    String windowName = "My HelloWorld Window"; //Name of the window
-
-    namedWindow(windowName); // Create a window
-
-    imshow(windowName, image); // Show our image inside the created window.
-
-    waitKey(0); // Wait for any keystroke in the window
-
-    destroyWindow(windowName); //destroy the created window
-
-    return 0;
+	computeImage(img, flippedImg);
+	cv::drawMatches(img, imgKeyPoints, flippedImg, flippedKeyPoints, filteredMatches, res);
+	cv::imshow("res", res);
+	cv::waitKey();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
