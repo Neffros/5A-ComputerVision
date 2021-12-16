8b9f12ea26ac69c04503b8e60b1dd1a5cccc82c6
Lior <diler.lior@gmail.com>
2021-12-16 09:10:38 +0100

    sort of functionnal binding box of objects but shown at the same time

--- features/features.cpp
+++ features/features.cpp
@@ -2,41 +2,79 @@
 //
 #include <iostream>
 #include <string>
+#include <filesystem>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+namespace fs = std::filesystem;
 using namespace cv;
 using namespace std;
 
+int nbORBtries = 2500;
+float distanceCoeff = 0.6;
+int minPointMatches = 4;
+
 cv::Mat nextInput;
+std::vector<cv::Mat> objects;
 cv::Mat object;
 
+std::vector<std::vector<cv::KeyPoint>> objectsKeyPoints;
 std::vector<cv::KeyPoint> objectKeyPoints;
+
 std::vector<cv::KeyPoint> sceneKeyPoints;
 
+std::vector<cv::Mat> objectsDescriptor;
 cv::Mat objectDescriptor;
 cv::Mat sceneDescriptor;
 
-cv::Ptr<cv::ORB> orb = cv::ORB::create();
+cv::Ptr<cv::ORB> orb = cv::ORB::create(nbORBtries);
 
 cv::BFMatcher bfMatcher;
 std::vector<std::vector<cv::DMatch>> matches;
 std::vector<cv::DMatch> filteredMatches;
 
+std::vector<std::vector<Point2f>> objectsEdges;
 std::vector<Point2f> objectEdges;
+
+std::vector<cv::Scalar> colors(5);
 
 cv::Mat output;
 
-void computeImage()
+std::vector<cv::Mat> getAllImagesInPath(std::string path)
+{
+    std::vector<Mat> images;
+    int i = 0;
+    std::cout << "getting images at path: "<< path << std::endl;
+    for (const auto& entry : fs::directory_iterator(path))
+    {
+        std::string ext = entry.path().extension().string();
+        if (ext != ".png" && ext != ".jpeg" && ext != ".BMP" && ext != ".TGA" && ext != ".jpg")
+        {
+            std::cout << entry.path().string().c_str() << " is not a compatible image" << std::endl;;
+            continue;
+        }
+        cv::Mat im;
+        
+        im = imread(entry.path().string().c_str());
+        images.push_back(im);
+        i++;
+    }
+    return images;
+}
+
+void computeImage(cv::Mat object)
 {
     orb->detect(object, objectKeyPoints);
     orb->compute(object, objectKeyPoints, objectDescriptor);
+
+    objectsKeyPoints.push_back(objectKeyPoints);
+    objectsDescriptor.push_back(objectDescriptor);
 }
 
-std::vector<cv::Point2f> getImageEdges(const cv::Mat image)
+void getImageEdges(const cv::Mat image)
 {
     std::vector<cv::Point2f> res(4);
 
@@ -45,28 +83,31 @@ std::vector<cv::Point2f> getImageEdges(const cv::Mat image)
     res[2] = cv::Point2f(image.cols, image.rows);
     res[3] = cv::Point2f(0, image.rows);
 
-    return res;
+    objectsEdges.push_back(res);
 }
-void drawBindingBox(cv::Mat& image)
+void drawBindingBox(cv::Mat& image, int index, bool enableOffset)
 {
     std::vector<Point2f> objectPoints;
     std::vector<Point2f> scenePoints;
     std::vector<Point2f> sceneEdges(4);
-
     for (auto match : filteredMatches)
     {
-        objectPoints.push_back(objectKeyPoints[match.queryIdx].pt);
-        scenePoints.push_back(sceneKeyPoints[match.trainIdx].pt);
+        objectPoints.push_back(objectsKeyPoints[index][match.trainIdx].pt); //reverse query and trainidx?
+        scenePoints.push_back(sceneKeyPoints[match.queryIdx].pt);
     }
-
     cv::Mat homography = cv::findHomography(objectPoints, scenePoints); //cv::RANSAC
-    cv::perspectiveTransform(objectEdges, sceneEdges, homography);
-
+    cv::perspectiveTransform(objectsEdges[index], sceneEdges, homography);
+    /*cv::Point2f offset = {0,0};
+    if (enableOffset)offset = Point2f(object.cols, 0);
     //Dessine sur le flux vidéo
-    cv::line(image, sceneEdges[0], sceneEdges[1], (255, 255, 255), 5);
-    cv::line(image, sceneEdges[1], sceneEdges[2], (255,255,255), 5);
-    cv::line(image, sceneEdges[2], sceneEdges[3], (255, 255, 255), 5);
-    cv::line(image, sceneEdges[3], sceneEdges[0], (255, 255, 255), 5);
+    sceneEdges[0] += offset;
+    sceneEdges[1] += offset;
+    sceneEdges[2] += offset;
+    sceneEdges[3] += offset;*/
+    cv::line(image, sceneEdges[0], sceneEdges[1], colors[index], 5);
+    cv::line(image, sceneEdges[1], sceneEdges[2], colors[index], 5);
+    cv::line(image, sceneEdges[2], sceneEdges[3], colors[index], 5);
+    cv::line(image, sceneEdges[3], sceneEdges[0], colors[index], 5);
 }
 void computeVideo()
 {
@@ -75,11 +116,12 @@ void computeVideo()
 
     orb->detect(nextInput, sceneKeyPoints);
     orb->compute(nextInput, sceneKeyPoints, sceneDescriptor);
-    bfMatcher.knnMatch(objectDescriptor, sceneDescriptor, matches, 2);
+
+    bfMatcher.knnMatch(sceneDescriptor, matches, 2);
 
     for (auto match : matches)
     {
-        if (match[0].distance < 0.65 * match[1].distance)
+        if (match[0].distance < distanceCoeff * match[1].distance)
         {
             filteredMatches.push_back(match[0]);
         }
@@ -98,10 +140,12 @@ void draw()
     }*/
 
     //cv::drawMatches(object, objectKeyPoints, nextInput, sceneKeyPoints, filteredMatches, img);
-    if (filteredMatches.size() > 6)
+    if (filteredMatches.size() > minPointMatches)
     {
-        std::cout << "binding box" << std::endl;
-        drawBindingBox(img);
+        for (auto i = 0; i < objects.size(); ++i)
+        {
+            drawBindingBox(img, i, false);
+        }
     }
 
 
@@ -130,37 +174,45 @@ void video(const std::string videoname = "")
     while (!nextInput.empty())
     {
         cap >> nextInput;
+
         computeVideo();
         draw();
+        //for auto object : objects do compute video and draw based on that object in parameter
+
         if (cv::waitKey(16) >= 0) break;
     }
 }
 
+void initColors()
+{
+    colors[0] = (255, 255, 255);
+    colors[1] = (255, 255, 0);
+    colors[2] = (255, 0, 0);
+    colors[3] = (0, 0, 255);
+    colors[4] = (0, 255, 0);
+}
 
 int main()
 {
-    std::cout << "object" << std::endl;
+
     std::string resourcePath = "E:/dev/vision_par_ordinateur/5A-ComputerVision/features/resources/set1/";
-    std::string objectPath = "bleach.jpg";
+    std::string objectPath = "naruto.jpg";
     std::string videoPath = "video.mp4";
-    // Read the image file
-    object = imread(resourcePath + objectPath);
 
-    if (object.empty()) // Check for failure
+    objects = getAllImagesInPath(resourcePath);
+    for (auto object : objects)
     {
-        cout << "Could not open or find the image" << endl;
-        system("pause"); //wait for any key press
-        return -1;
+        getImageEdges(object);
+        computeImage(object);
     }
-    computeImage();
+    initColors();
 
-    for (auto keyPoint : objectKeyPoints)
-    {
-        cv::circle(object, keyPoint.pt, 5, cv::Scalar(255, 255, 255));
-    }
-    cv::imshow("object", object);
-    objectEdges = getImageEdges(object);
+    bfMatcher.add(objectsDescriptor);
+    bfMatcher.train();
+    //cv::waitKey();
+
     video(resourcePath + videoPath);
+    cv::waitKey();
     //waitKey(0); // Wait for any keystroke in the window
 
     return 0;
---
+++ symetry/symetry.cpp
@@ -0,0 +1,50 @@
+// 5A-ComputerVision.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+int main()
+{
+    std::cout << "PROJECT1" << std::endl;
+    std::string projectPath = "E:/dev/vision_par_ordinateur/5A-ComputerVision/disparity/resources/antoine.png";
+    // Read the image file
+    Mat image = imread(projectPath);
+
+    if (image.empty()) // Check for failure
+    {
+        cout << "Could not open or find the image" << endl;
+        system("pause"); //wait for any key press
+        return -1;
+    }
+
+    String windowName = "My HelloWorld Window"; //Name of the window
+
+    namedWindow(windowName); // Create a window
+
+    imshow(windowName, image); // Show our image inside the created window.
+
+    waitKey(0); // Wait for any keystroke in the window
+
+    destroyWindow(windowName); //destroy the created window
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
